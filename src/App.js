bca8b4fce48e42717b73a62e1ee3e5c78e639ba7
Ayesha Ghani <60398800+ayesha-ghani098@users.noreply.github.com>
2020-09-03 18:19:38 +0500

    practicing react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    // return <div className="content"><strong>Hello Ayesha</strong> <ul><li>Class: BSCS</li><li>Age: {18+2}</li></ul></div>;
+
+
+ //------------ Greeting  Component exercise----------//
+    // return <div>Hello {props.fname} {props.lname}! </div>
+
+
+//----------- Media Card Component exercise---------//
+    // return <div>
+    //     <img src="{props.url}"></img>
+    //     <h2>
+    //        {props.title}
+    //     </h2>
+    //     <p>
+    //         {props.body}
+    //     </p>
+    // </div>
+
+
+    //---------- Gate Component exercise---------//
+    // return <div>
+    //     {props.isOpen ? "Open":"Closed"}
+    // </div>
+
+    const [islit,setlit] = useState(true);
+
+ //--------Background on/off Cmoponent------//
+ return <div className="room">THE ROOM IS {islit? 'LIT' : 'DARK'}</div>   
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( 
+    // <App url="images/1.jpg" title="Navy Blue Jacket" body="Opt for this super-practical zip-up jacket in navy blue sporty bomber jacket made with knitted fabric. Made with light outer fabric, it guarantees sporty look. Featuring baseball ribbed collar & cuffs with vertical hand pockets. Must have for a sporty look
+    // Product Code : JKT-086
+    // Outer: 100% Cotton, Lining: 65% Polyester, 35% Cotton"/>,
+
+    <App isOpen={false} />,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
